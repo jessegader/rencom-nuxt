142e106c32f9488511d7e69ba4e61fd7927168a4
jessegader <jessegader@gmail.com>
2023-09-04 01:08:50 -0400

    icons

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,34 @@
 <template lang='pug'>
-.navbar.bg-neutral 
-    .btn.btn-primary {{ news[0].title }}
-.toast.toast-center.toast-middle
-    .alert.alert-info
-        span {{ news[0].desc }}
+.min-h-screen.flex.flex-col
+    header.navbar.bg-neutral
+        Bars3Icon.icon.h-9.w-9.text-base
+        .btn.btn-primary(@click='showToast') {{ news[0].desc }}
+    section.flex-1.flex.flex-col(class='sm:flex-row')
+        main.flex-1.p-2
+            .toast.toast-center.toast-middle(v-show='show' )
+                .alert.alert-info
+                    span {{ news[0].desc }}
+    footer.footer.bg-neutral.p-10
 </template>
 
 <script setup>
 const { getItems } = useDirectusItems()
-
 const news = await getItems({
     collection: "news"
 })
+import { ref } from 'vue'
+import { Bars3Icon } from '@heroicons/vue/24/solid'
+// state
+const show = ref(false)
+
+// actions
+function showToast() {
+    show.value = !show.value
+}
+
+setTimeout(() => {
+    showToast()
+}, 1500)
 </script>
 
 <style lang="sass">
@@ -21,4 +38,6 @@ const news = await getItems({
     font-weight: 600
     text-transform: uppercase
     padding: 1rem 1.5rem
+.btn
+    margin: 0.25rem 0 0.25rem auto
 </style>

8fa41b02b9942cccf812fad2461edf3efe62dd28
jessegader <jessegader@gmail.com>
2023-09-04 02:20:48 -0400

    daisy

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,20 @@
 <template lang='pug'>
 .min-h-screen.flex.flex-col
-    header.navbar.bg-neutral
-        Bars3Icon.icon.h-9.w-9.text-base
-        .btn.btn-primary(@click='showToast') {{ news[0].desc }}
+    header.navbar.bg-neutral.shadow-down.z-10
+        .btn.btn-ghost(@click='checkBox')
+            Bars3Icon.icon.h-9.w-9.text-base
+        .btn.btn-primary.left(@click='showToast') {{ news[0].desc }}
     section.flex-1.flex.flex-col(class='sm:flex-row')
-        main.flex-1.p-2
-            .toast.toast-center.toast-middle(v-show='show' )
-                .alert.alert-info
-                    span {{ news[0].desc }}
-    footer.footer.bg-neutral.p-10
+        main.flex-1.z-0
+            .drawer
+                input#my-drawer.drawer-toggle(type='checkbox')
+                .drawer-side
+                    ul.menu.p-4.w-80.h-full.bg-neutral.text-base-content
+                .drawer-content
+                    .toast.toast-center.toast-middle(v-show='show' )
+                        .alert
+                            label.btn.btn-info(for='my-drawer') {{ news[0].desc }}
+    footer.footer.bg-neutral.p-10.shadow-up.z-10
 </template>
 
 <script setup>
@@ -16,14 +22,20 @@ const { getItems } = useDirectusItems()
 const news = await getItems({
     collection: "news"
 })
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { Bars3Icon } from '@heroicons/vue/24/solid'
+
 // state
 const show = ref(false)
+
 
 // actions
 function showToast() {
     show.value = !show.value
+}
+function checkBox() {
+    const check = document.querySelector('#my-drawer')
+    check.checked = !check.checked
 }
 
 setTimeout(() => {
@@ -38,6 +50,17 @@ setTimeout(() => {
     font-weight: 600
     text-transform: uppercase
     padding: 1rem 1.5rem
-.btn
+    background-color: #272935
+    border: none
+.left
     margin: 0.25rem 0 0.25rem auto
+.drawer
+    height: 100%
+    .drawer-side
+        position: absolute
+        height: 100%
+.shadow-up
+    box-shadow: (0 -4px 4px 1px rgb(0 0 0 / 0.08))
+.shadow-down
+    box-shadow: (0 4px 4px 1px rgb(0 0 0 / 0.08))
 </style>

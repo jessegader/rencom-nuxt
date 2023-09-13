97d7bc89370ef80210419301947b19b715585d66
jessegader <jessegader@gmail.com>
2023-09-12 20:15:50 -0400

    undo csp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: false },
-	modules: ['nuxt-directus', '@pinia/nuxt', 'nuxt-security'],
+	modules: ['nuxt-directus', '@pinia/nuxt'],
 	directus: {
-		url: 'https://api.rencom.xyz'
-	},
-	security: {
-		headers: {
-			contentSecurityPolicy: {
-				'base-uri': ["'self'"],
-				'font-src': ["'self'", 'https:', 'data:'],
-				'form-action': ["'self'"],
-				'frame-ancestors': ["'self'"],
-				'frame-src': ["'self'"],
-				'img-src': ["'self'", 'data:'],
-				'object-src': ["'none'"],
-				'script-src-attr': ["'none'"],
-				'style-src': ["'self'", 'https:', "'unsafe-inline'"],
-				'upgrade-insecure-requests': true
-			  }
-		}
+		url: 'https://api.rencom.xyz',
 	},
 	ssr: false,
 	app: {

502d8b7c987adf5c512f9be967f7d6fd4acdce56
jessegader <jessegader@gmail.com>
2023-09-12 20:05:34 -0400

    add csp ?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: false },
-	modules: ['nuxt-directus', '@pinia/nuxt'],
+	modules: ['nuxt-directus', '@pinia/nuxt', 'nuxt-security'],
 	directus: {
-		url: 'https://api.rencom.xyz',
+		url: 'https://api.rencom.xyz'
+	},
+	security: {
+		headers: {
+			contentSecurityPolicy: {
+				'base-uri': ["'self'"],
+				'font-src': ["'self'", 'https:', 'data:'],
+				'form-action': ["'self'"],
+				'frame-ancestors': ["'self'"],
+				'frame-src': ["'self'"],
+				'img-src': ["'self'", 'data:'],
+				'object-src': ["'none'"],
+				'script-src-attr': ["'none'"],
+				'style-src': ["'self'", 'https:', "'unsafe-inline'"],
+				'upgrade-insecure-requests': true
+			  }
+		}
 	},
 	ssr: false,
 	app: {
